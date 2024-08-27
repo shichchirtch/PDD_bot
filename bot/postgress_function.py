89bb13f7de4b4d0ccc2323062a4d016f41b40ce3
shichchirtch <ivanvol9l@gmail.com>
2024-08-27 20:09:11 +0200

    13 COMMIT
    Билеты 15-16

--- bot/postgress_function.py
+++ bot/postgress_function.py
@@ -6,7 +6,7 @@ async def insert_new_user_in_table(user_tg_id: int, name: str):
         query = await session.execute(select(User).filter(User.tg_us_id == user_tg_id))
         needed_data = query.scalar()
         if not needed_data:
-            print('Now we are into first function')
+            # print('Now we are into first function')
             new_us = User(tg_us_id=user_tg_id, user_name=name)
             session.add(new_us)
             await session.commit()
@@ -15,7 +15,7 @@ async def insert_new_user_in_table(user_tg_id: int, name: str):
 async def check_user_in_table(user_tg_id:int):
     """Функция проверяет есть ли юзер в БД"""
     async with session_marker() as session:
-        print("Work check_user Function")
+        # print("Work check_user Function")
         query = await session.execute(select(User).filter(User.tg_us_id == user_tg_id))
         data = query.one_or_none()
         return data
@@ -24,7 +24,7 @@ async def insert_new_ticket(user_tg_id: int, ticket:list):
     async with session_marker() as session:
         query = await session.execute(select(User).filter(User.tg_us_id == user_tg_id))
         needed_data = query.scalar()
-        print('works insert_new_ticket')
+        # print('works insert_new_ticket')
         needed_data.frage_list = ticket
         await session.commit()
 
@@ -32,7 +32,7 @@ async def total_increment(user_tg_id: int):
     async with session_marker() as session:
         query = await session.execute(select(User).filter(User.tg_us_id == user_tg_id))
         needed_data = query.scalar()
-        print('works insert_stars')
+        # print('works insert_stars')
         needed_data.total += 1
         await session.commit()
 
